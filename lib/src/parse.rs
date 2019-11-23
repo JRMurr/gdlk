7f9435f3e2593aa17c8b28bd4eb8c7990e4ee1ac
John Murray <5672686+JRMurr@users.noreply.github.com>
2019-11-23 13:36:16 -0500

    start nom

--- lib/src/parse.rs
+++ lib/src/parse.rs
@@ -1,14 +1,23 @@
 use crate::ast::Program;
 use failure::Fallible;
+use nom::IResult;
 use std::io::Read;
 
+fn do_parse(input: &str) -> IResult<&str, Program> {
+    // Ok((input, Program { body: vec![] }))
+    match serde_json::from_str(&input) {
+        Ok(program) => Ok((input, program)),
+        _ => panic!("ass2"),
+    }
+}
 impl Program {
     /// Parses source code from the given input, into an abstract syntax tree.
     pub fn parse(source: &mut impl Read) -> Fallible<Self> {
         let mut source_buffer = String::new();
         source.read_to_string(&mut source_buffer)?;
-        // TODO real parsing here
-        let program = serde_json::from_str(&source_buffer)?;
-        Ok(program)
+        match do_parse(&source_buffer) {
+            Ok((_, program)) => Ok(program),
+            _ => panic!("ass"),
+        }
     }
 }
